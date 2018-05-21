9c1d3270f3a343380eaf3a09586a86de2b337468
Deborah Vanzin <debi_vanzin@hotmail.com>
2018-05-21 07:20:18 +0000

    Diverses

---
+++ db/migrate/20180520214444_add_username_and_name_to_user.rb
@@ -0,0 +1,5 @@
+class AddUsernameAndNameToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520134616) do
+ActiveRecord::Schema.define(version: 20180520214444) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180520134616) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
